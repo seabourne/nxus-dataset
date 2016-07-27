13d507ad3bf3590228f179a4dc154140a8b09dc3
ScottM <scott@zebrasoftware.net>
2016-07-27 15:24:19 -0700

    Richer field object in DataSet, Compatbility fixes:  * process datarows coming in to gather guesses at a name and type for dataset field array member  * better ID assignment for the created dataset field array member  * allow backward compatbility on the view-data pages for old datasets that still use the string-array for dataset.fields (transition only)  * edit/create view and backing logic for creating and changing a dataset record

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ export default class DataSets {
       return this.view(opts.req)
     })
 
-    this.loader.on('model.datarow', this._processDataRow.bind(this))
+    this.loader.on('model.datarow', this._processLoaderDataRow.bind(this))
 
     this.app.get('base-ui').viewModel('dataset', {display: ['name', 'source', 'description']})
 
@@ -50,37 +50,31 @@ export default class DataSets {
     this.admin.adminPage('Upload Data', 'datasets/:id/data', {nav: false}, this.dataUpload.bind(this))
     this.admin.adminPage('View Data', 'datasets/:id/view', {nav: false}, this.view.bind(this)) 
 
-    this.admin.adminRoute('post', 'datasets/:id/data/save', this.saveDataUpload.bind(this)) //workaround - url is different for post
+    this.admin.adminRoute('post', 'datasets/:id/data/save', this.saveDataUpload.bind(this)) //save datarows for dataset - url is different for post
+    this.admin.adminRoute('post', 'datasets/:id/dataset/save', this.saveDataSet.bind(this)) //update dataset, from custom form (see views/admin-dataset-form)
+    this.admin.adminRoute('post', 'datasets/dataset/save', this.saveDataSet.bind(this)) //new dataset, from custom form (see views/admin-dataset-form)
 
     this.admin.replace().adminRoute('get', 'datasets/:id/remove',  this.removeDataRowsForSet.bind(this)) //intercept admin-ui default dataset delete 
-    // this.admin.replace().adminRoute('get', 'datasets/:id/edit', this.editDataSet.bind(this))
     
     this.loader.uploadPath('/admin/datasets/:id/data/save', 'csvfile')
   }
 
   /**
-   * Handler for data rows to clean up values.
-   * @param  object row is the row of data with CSV column-header keys
+   * Handler for data rows to clean up records; attached to loader's 'model.datarow'.
+   * @param  object row is the row of data from CSV column-header keys
    */
-  _processDataRow(row) {
-    let cleanRow = _.mapObject(row, (val,key) => {
-      if (typeof(val) != 'undefined') {
-        let inferredType = mconst.DECIMAL_TYPE
-        if (val.endsWith('%')) {
-          let what = Number.parseFloat(val)
-          if (_.isNumber(what) && !Number.isNaN(what)) {
-            val = what
-          }
-          inferredType = mconst.PERCENT_TYPE
-        }
-        return val
-      } else {
-        this.app.log.debug( "_processDataRow undefined at:", key)
-        return ''
-      }
-    })
-    this.app.log.debug( "_processDataRow cleanRow:", cleanRow)
-    return cleanRow
+  _processLoaderDataRow(row) {
+    //catch CSV column headers that would get 
+    //over-ridden or conflict downstream.
+    if (row.id) {
+      row.id_orig = row.id
+      delete row.id
+    }
+    if ( row.dataset) {
+      row.dataset_orig = row.dataset
+      delete row.dataset
+    }
+    return row
   }
 
   dataUpload(req, res) {
@@ -108,9 +102,66 @@ export default class DataSets {
     })
   }
 
+  /**
+   * clean up the datarow after storage,
+   * associating with the given dataset ID.
+   * @param  {[datarow]} row       DataRow instance to put under given DataSet ID
+   * @param  {[type]} dataSetId [description]
+   * @return the original row passed in, with some property modifications.
+   */
+  _prepareDataRowInDataSet(row, dataSetId) {
+    _.each(row, (val,key) => {
+      if (typeof(val) == 'string') {
+        if (val.endsWith('%')) {
+          let what = Number.parseFloat(val)
+          if (_.isNumber(what) && !Number.isNaN(what)) {
+            val = what
+          }
+        }
+      } else if (typeof(val) == 'undefined') {
+        val = null
+      }
+      row[key] = val
+    })
+    row.dataset = dataSetId
+    //nb: important to return changed row rather than new object
+    return row
+  }
+
+  /**
+   * put in a data type for each field in uploaded datarow
+   * @param  {[datarow]} sample [representative row of data]
+   * @return {[object]}        [map by datarow key of best estimate for the type]
+   */
+  _buildIntialDataTypes(sample) {
+    let typeMap = {}
+    _.each(sample, (val,key) => {
+      if (typeof(val) == 'string') {
+        if (val.endsWith('%')) {
+          typeMap[key] = mconst.PERCENT_TYPE
+        } else if (! isNaN(parseFloat(val))) {
+          typeMap[key] = mconst.DECIMAL_TYPE
+        } else {
+          typeMap[key] = mconst.STRING_TYPE
+        }
+      }
+      if ( typeMap[key] != mconst.PERCENT_TYPE && 
+        ((-1 < key.toLowerCase().indexOf('percent')) || (-1 < key.indexOf('%'))) ) {
+        typeMap[key] = mconst.PERCENT_TYPE
+      }
+    })
+    return typeMap
+  }
+
+  /**
+   * Save datarows uploaded from CSV,
+   * associating each datarow with the parent dataset
+   * and gathering summary field information about the collection of rows
+   * to store in the parent.
+   * 
+   */
   saveDataUpload(req, res) {
     let setId = req.param('id')
-    let currentFields = []
     if(!req.file) {
       req.flash('error', "Error processing rows: no file supplied for the data.")
       return res.redirect('/admin/datasets/'+setId+'/data')
@@ -118,12 +169,19 @@ export default class DataSets {
     return this.loader.importFileToModel('datarow', req.file.path, {type: 'csv', strict: false})
     .then((rows) => {
       let inferredTypeMap = {}
+      let currentFields = []
       _.each(rows, (rowElem) => {
-        this.app.log.debug("saveDataUpload processing row rowElem", rowElem)
+        // this.app.log.debug("saveDataUpload processing rowElem", rowElem)
+        if (Array.isArray(rowElem)) {
+          rowElem = rowElem[0] //work-around for model handler dupe bug in data-loader 3.0.0
+        }
+        if (_.isEmpty(inferredTypeMap)) {
+          inferredTypeMap = this._buildIntialDataTypes(rowElem)
+          currentFields = _.keys(rowElem)
+        }
         if (setId) {
-          rowElem.dataset = setId
-          currentFields = _.unique(_.compact(currentFields.concat(_.keys(rowElem))))
-          rowElem.save().catch((err) => {
+          let rowToUpdate = this._prepareDataRowInDataSet(rowElem, setId)
+          rowToUpdate.save().catch((err) => {
             this.app.log.error("nxus-dataset update failed for datarow " + rowElem.id, err)
           })
         }
@@ -131,9 +189,10 @@ export default class DataSets {
       return this.storage.getModel('dataset').then((DataSet) => {
         return DataSet.findOne(setId)
       }).then((set) => {
-        let fieldList = _.map(_.without(currentFields, 'id', 'dataset'), (columnName) => {
+        let fieldList = _.map(_.without(currentFields, 'id', 'dataset', 'true'), (columnName) => {
           let genId = this._generateUniqueId()
-          let newField = { name: columnName, id: genId }
+          let newField = { name: columnName, id: genId, label: morph.toTitle(columnName)}
+          if (inferredTypeMap[columnName]) newField.type = inferredTypeMap[columnName]
           return newField
         })
         set.rowCount = rows.length
@@ -145,12 +204,16 @@ export default class DataSets {
         })
       })
     }).catch((e) => {
-      this.app.log.error( "error loading data rows  ", e, " at: ", e.stack);
+      this.app.log.error( "nxus-dataset saveDataUpload() error loading data rows  ", e, " at: ", e.stack);
       req.flash('error', "Error processing rows")
       return res.redirect('/admin/datasets/'+setId+'/data')
     })
   } 
 
+  /**
+   * Action when a dataset is removed -
+   * removes the dataset and all child datarows.
+   */
   removeDataRowsForSet(req, res) {
     let dataSetId = req.param('id');
     return this.storage.getModel('datarow')
@@ -172,26 +235,62 @@ export default class DataSets {
     })
   }
 
-  editDataSet(req, res) {
-    let dataSetId = req.param('id');
-    let opts = {}
-    this.app.log.debug("editDataSet " + dataSetId)
-    return this.storage.getModel('dataset')
-    .then( (dataSetModel) => {
-      return dataSetModel.findOne(dataSetId)
-    }).then( (foundDataSet) => {
-      opts.id = dataSetId
-      opts.dataset = foundDataSet
-      this.app.log.debug("editDataSet p[ts" , opts)
-      return this.templater.render('edit-dataset-form', opts)
-    }).catch( (err) => {
-      this.app.log.error("nxus-dataset error on edit of dataset " + dataSetId, err.stack)
-      req.flash('error', "Error editing dataset " + err)
-      return res.redirect('/admin/datasets/')
-    })
+  /**
+   * handle POST response for edit existing 
+   * or create new dataset
+   */
+  saveDataSet(req, res) {
+    let dataSetId = req.params.id
+    this.app.log.debug("saveDataSet id: ", dataSetId)
+    if (dataSetId) {
+      
+      this.storage.getModel('dataset').then((dsmodel) => {
+        return dsmodel.findOne(dataSetId)
+      }).then((dataset) => {
+        dataset.name = req.body.name
+        dataset.source = req.body.source
+        dataset.description = req.body.description
+        _.each( req.body, (value,key) => {
+          //update fields from request parameters field_<field.id>_<key>
+          if (0 <= key.indexOf('field_')) {
+            let pComponents = key.split('_')
+            if ( 3 == pComponents.length) {
+              let fieldId = pComponents[1]
+              let fieldKey = pComponents[2]
+              let targetField = _.findWhere(dataset.fields, {id: fieldId})
+              if (targetField) {
+                targetField[fieldKey] = value
+              }
+            } 
+          }
+        })
+        return dataset.save().catch( (err) => {
+          this.app.log.error("nxus-dataset: dataset update failed on " + dataSetId, err.stack)
+          return res.redirect('/admin/datasets')
+        }).then( () => {
+          return res.redirect('/admin/datasets')
+        })
+      })
+    } else {
+      return this.storage.getModel('dataset')
+      .then( (dataSetModel) => {
+        if ( req.body.name) {
+          return dataSetModel.create({name: req.body.name, source: req.body.source, description: req.body.description})
+          .then( (newRecord) =>{
+            this.app.log.debug("dataset created ", newRecord.id)
+            return res.redirect('/admin/datasets')
+          })
+        } else {
+          req.flash('error', "Name is required")
+          return res.redirect('/admin/datasets/create')
+        }
+      })
   }
+}
 
  _generateUniqueId() {
+  //maybe overkill for use in cases here,
+  // but it's unique across launches of the app
     return nodeUUID.v1();
  }
 
